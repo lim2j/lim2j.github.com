ab47f96ad0c9dd876aaf3dad6b881eb85dd5c662
lim2j <rap1006@nate.com>
2015-08-26 10:31:55 +0900

    10

--- admin/js/custom.js
+++ admin/js/custom.js
@@ -271,6 +271,22 @@ function countChecked() {
     /** ******    *********************** **/
     /** ******    *********************** **/
     /** ******  Accordion  *********************** **/
+jQuery(function ($) {
+    $('[data-toggle="collapse"]').on('click', function() {
+        var $this = $(this),
+                $parent = typeof $this.data('parent')!== 'undefined' ? $($this.data('parent')) : undefined;
+        if($parent === undefined) { /* Just toggle my  */
+            $this.find('.fa').toggleClass('fa-plus fa-minus');
+            return true;
+        }
+
+        /* Open element will be close if parent !== undefined */
+        var currentIcon = $this.find('.fa');
+        currentIcon.toggleClass('fa-plus fa-minus');
+        $parent.find('.fa').not(currentIcon).removeClass('fa-minus').addClass('fa-plus');
+
+    });
+});
 
 $(function () {
     $(".expand").on("click", function () {
