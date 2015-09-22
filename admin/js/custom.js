cdbd1895f03a5e892b88d093dddca7e73efba951
lim2j <rap1006@nate.com>
2015-09-22 13:04:22 +0900

    ui+

--- admin/js/custom.js
+++ admin/js/custom.js
@@ -7,6 +7,36 @@
 /** ******  left menu  *********************** **/
 $(function () {
 
+    $(window).resize(function(){
+
+       if($(window).width() < "1280"){
+           if ($('body').hasClass('nav-md')) {
+               $('body').removeClass('nav-md');
+               $('body').addClass('nav-sm');
+               $('.left_col').removeClass('scroll-view');
+               $('.left_col').removeAttr('style');
+               $('.sidebar-footer').hide();
+
+               if ($('#sidebar-menu li').hasClass('active')) {
+                   $('#sidebar-menu li.active').addClass('active-sm');
+                   $('#sidebar-menu li.active').removeClass('active');
+               }
+
+           }
+       } else {
+
+           $('body').removeClass('nav-sm');
+           $('body').addClass('nav-md');
+           $('.sidebar-footer').show();
+
+           if ($('#sidebar-menu li').hasClass('active-sm')) {
+               $('#sidebar-menu li.active-sm').addClass('active');
+               $('#sidebar-menu li.active-sm').removeClass('active-sm');
+           }
+
+       }
+   });
+
 
     $('#sidebar-menu li ul').slideUp();
     $('#sidebar-menu li').removeClass('active');
