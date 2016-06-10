931dfa9584fc96c0090513ed40bda31323820822
lim2j <rap1006@nate.com>
2016-06-10 16:54:04 +0900

    main exmple

--- mobilelessjs/js/jj.js
+++ mobilelessjs/js/jj.js
@@ -65,6 +65,7 @@
 
             $el.addClass(options.LeftOpenClass);
             $('.'+options.screenBg).show();
+
             console.log('openLeft');
         };
 
@@ -74,6 +75,7 @@
 
             $el.addClass(options.RightOpenClass);
             $('.'+options.screenBg).show();
+
             console.log('openRight');
         };
 
@@ -106,145 +108,9 @@
         });
     };
 
-
-
-    /* IMAGE SLIDER
-    PAGEING
-    */
-    $.jjImagesSlider = function(element, options){
-
-        var options = $.extend({}, $.jjImagesSlider.defaults, options);
-        // 첫번째 인자값이 {} 빈 객체이므로 defaults 객체의 멤버와 opts(사용자정의 옵션값)이 merge되어 options에 담겨진다.
-
-        var self = this;
-        var $el = $(element);
-        var sliderWrap = $(element).children('ol')
-        var sliderH = $el.find('img').height();
-        var sliderW = $el.width();
-        var sliderL = $el.find('img').length;
-        var sliderWrapW = sliderL * sliderW;
-        var sliderInterval;
-
-        // event
-        $('body').on('click', options.prevBtn, function(e){
-            self.moveLeft();
-            e.preventDefault();
-        });
-
-        $('body').on('click', options.nextBtn, function(e){
-            self.moveRight();
-            e.preventDefault();
-        });
-
-        self.slideAuto = function(){
-            var delay = $el.attr("data-delay") || options.delay;
-
-            sliderInterval = setInterval(function(){
-                self.moveRight();
-            }, delay);
-            console.log('auto');
-        };
-
-        self.slideStop = function(){
-            clearInterval(sliderInterval);
-            console.log('stop');
-        };
-
-        self.moveLeft = function(){
-            sliderWrap.animate({
-               left: +sliderW
-            }, 300, function(){
-                sliderWrap.find('li:last-child').appendTo(sliderWrap);
-                sliderWrap.css('left', '');
-            });
-
-            console.log('moveLeft');
-        };
-
-        self.moveRight = function(){
-            sliderWrap.animate({
-               left: -sliderW
-            }, 300, function(){
-                sliderWrap.find('li:first-child').appendTo(sliderWrap);
-                sliderWrap.css('left', '')
-            });
-            console.log('moveRight');
-        };
-
-        self.pagingActive = function(){
-
-        };
-
-        self.sliderSetting = function(){
-
-        };
-
-        self.init = function(){
-
-            $el.css({
-                'height': sliderH,
-                'width': sliderW
-            });
-            sliderWrap.css({
-                'width': sliderWrapW,
-                'marginLeft': -sliderW
-            });
-            sliderWrap.find('li').css({
-                'height': sliderH,
-                'width': sliderW,
-            });
-            sliderWrap.find('li:last-child').prependTo(sliderWrap);
-
-            //pagination
-            if(options.pagination){
-                var a = '<a href="#"></a>';
-                var pagingP = $el.attr('data-paging-positon') || option.pagingPositon;
-                $el.append($('<div/>',{
-                        'class':options.pagingClass,
-                        'style':'bottom:'+pagingP+'px'
-                    })
-                );
-                for(var i = 0; i < sliderL; i++){
-                    $el.find('.'+options.pagingClass).append(a);
-                }
-
-            }
-
-            //auto
-            if(options.auto){
-                self.slideAuto();
-            }
-        };
-
-        self.init();
-
-        // resize
-        $(window).resize(function(){
-
-        });
-    };
-
-    /* defaults optipon */
-    $.jjImagesSlider.defaults = {
-        prevBtn:'slider-prevbtn',
-        nextBtn:'slider-nextbtn',
-        auto: true,
-        delay: '5000',
-        pagination: true,
-        pagingClass: 'slider-pagination',
-        pagingPositon: '50'
-    };
-
-    /* plugin */
-    $.fn.jjImagesSlider = function(options){
-        return this.each(function(){
-            var element = $(this);
-            var jjImagesSlider = new $.jjImagesSlider(this, options);
-            element.data('jjImagesSlider', jjImagesSlider);
-        });
-    };
-
 })(jQuery);
+
+
 
 $(function(){
     // 플러그인의 defaults 값을 외부에서 변경할 수 있다.
@@ -252,8 +118,20 @@ $(function(){
 
     // 사용자 정의의 옵션값을 정의하여 플러그인 메소드를 호출한다.
     // slide nav
-    var slidenav = new $("#menu").jjSlideNav({});
+    var slidenav = new $('#menu').jjSlideNav({});
 
     // slider
-    var imageslider = new $('.slider').jjImagesSlider({});
+    $(".slider ol").bxSlider({
+        auto: true,
+        pause: $('.slider').attr('data-delay'),
+        controls: false,
+        onSliderLoad : function(){
+            var pagingPosition = $(".slider").attr("data-paging-position");
+
+            if(pagingPosition != undefined){
+                $('.slider').find('.bx-pager').css('bottom', pagingPosition+'px');
+            }
+        }
+    });
+
 });
